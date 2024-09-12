f9f694ac8c3e067c64c6837cc5fc9cb3d1544d59
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-12 17:20:07 +0000

    a# Please enter the commit message for your changes. Lines starting

--- PP1_1.py
+++ PP1_1.py
@@ -2,19 +2,15 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-  print()
 
 def q2():
   #Write Assignment code here
-  print()
   
 def q3():
   #Write Assignment code here
-  print()
   
 def q4():
   #Write Assignment code here
-  print()
 
 #Do not alter anything after this line
 if __name__ == "__main__":
