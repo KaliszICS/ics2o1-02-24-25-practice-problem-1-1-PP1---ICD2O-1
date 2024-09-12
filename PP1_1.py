2edfea7e6bfdef7512356801066dd4cf3ce02cff
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-12 17:15:26 +0000

    test commit

--- PP1_1.py
+++ PP1_1.py
@@ -2,18 +2,23 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
+  print()
 
 def q2():
   #Write Assignment code here
+  print()
   
 def q3():
   #Write Assignment code here
+  print()
   
 def q4():
   #Write Assignment code here
+  print()
 
 #Do not alter anything after this line
-q1()
-q2()
-q3()
-q4()
+if __name__ == "__main__":
+  q1()
+  q2()
+  q3()
+  q4()
